c5f42168ec337615545aadf2f6b6c54b62fc35d1
Daniel Palmer <danielp@backbase.com>
2017-09-19 10:52:10 +0100

    Added routing via Eureka Service Registry

--- src/main/java/io/danpalmer101/gateway/Application.java
+++ src/main/java/io/danpalmer101/gateway/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * Spring Boot based Zuul Gateway proxy
  */
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "io.danpalmer101.gateway")
